2f250c9d4d64964bed3be3211c253a56e58437b7
daixi <daixi859@gmail.com>
2018-03-20 22:47:58 +0800

    添加项目

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { Button } from 'antd'
-import './App.less';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Primary</Button>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    document.getElementById('root')
-  )
-};
-
-
-render(App);
-if (module.hot) {
-  module.hot.accept('./App', () => { render(App) })
-}
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
