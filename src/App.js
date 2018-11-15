845a7f8f1bc01e83cac40d92f0adbd0670691028
戴曦 <daix@mingyuanyun.com>
2018-11-15 15:46:23 +0800

    安装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Button } from 'antd'
-import './App.less';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Primary</Button>
+        app
       </div>
     );
   }
---
+++ src/hexagonal/utils/hexMetrics.js
@@ -0,0 +1,2 @@
+export const outerRadius = 10;
+export const innerRadius = outerRadius * 0.866025404;
